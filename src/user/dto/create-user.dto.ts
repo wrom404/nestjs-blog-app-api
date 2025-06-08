5fac363cc75ba426de88372b8e33bd76a9dd8abc
Unkownymous99 <romwendel99@gmail.com>
2025-06-08 23:44:22 +0800

    feat: add user rest api and validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+// create-user.dto.ts
+import { Role } from '@prisma/client';
+import { IsEnum, IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: "Name must be at least 2 characters long." })
+  @MaxLength(50, { message: "Name cannot be longer than 50 characters." })
+  name: string;
+
+  @IsEmail()
+  @MaxLength(250)
+  email: string;
+
+  @IsEnum(Role, { message: `Role must be one of: ${Object.values(Role).join(', ')}` })
+  role?: Role = Role.USER; // // Default to USER if not provided
+}
